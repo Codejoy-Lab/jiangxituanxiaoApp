a30e73f1451bc2b0d8d16b0096ebe6453587859e
jianyi <jianyi@codejoy.cn>
2024-07-01 18:58:26 +0800

    feat: 添加webview

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import {View, StyleSheet, Dimensions, Text} from 'react-native';
-import {WebView} from 'react-native-webview';
+import {View, StyleSheet, Dimensions, Text, Image} from 'react-native';
+import {WebView, WebViewMessageEvent} from 'react-native-webview';
 
 export default () => {
   console.log('init');
-  
+  const handleMessage = (event: WebViewMessageEvent) => {
+    console.log('1');
+    const data = JSON.parse(event.nativeEvent.data);
+    console.log('Received from H5:', data);
+  };
+  const injectedJS = `(function() {
+    window.postMessage = function(data) {
+     
+      window.ReactNativeWebView.postMessage(JSON.stringify(data));
+    };
+  })();`;
   return (
     <View style={styles.container}>
       <WebView
-        source={{uri: 'http://192.168.44.98:5173/map'}}
-        style={styles.container}></WebView>
+        source={{
+          uri: 'http://192.168.44.85:5174/videoMapControlPage/map',
+        }}
+        injectedJavaScript={injectedJS} // 注入JS，确保H5页面能使用postMessage
+        onMessage={handleMessage} // 监听从H5发送过来的消息
+        style={styles.container}
+        originWhitelist={['*']}
+        javaScriptEnabled={true}
+      />
     </View>
   );
 };
@@ -18,6 +35,6 @@ const styles = StyleSheet.create({
     //  flex: 1,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
-    backgroundColor: 'red',
+    backgroundColor: '#fff',
   },
 });
